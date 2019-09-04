df8bd73807ca65d245ea9949e0685a3ea5bb26c4
Adil Dev <adilbekmomunaliev@Adilbeks-MacBook-Pro.local>
2019-09-03 19:37:52 -0700

    Updates

---
+++ precache-manifest.54b595a1c1111b3b611e4ed3e1e7dd90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "768dabb50d2d6d4dab4d3c9f779ce7f0",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "0664940f88a98b5832dc",
+    "url": "/website/static/css/main.4e222980.chunk.css"
+  },
+  {
+    "revision": "74f8b12a6de18accf1c6",
+    "url": "/website/static/js/2.cbeab657.chunk.js"
+  },
+  {
+    "revision": "0664940f88a98b5832dc",
+    "url": "/website/static/js/main.82f1b207.chunk.js"
+  },
+  {
+    "revision": "4289eddbccb8920ec418",
+    "url": "/website/static/js/runtime~main.d5dc7e65.js"
+  }
+]);
