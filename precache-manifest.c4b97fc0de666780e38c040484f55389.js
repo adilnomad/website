44c4c072f21065aca11d59f4e48303816f1c6d09
Adil Dev <adilbekmomunaliev@Adilbeks-MacBook-Pro.local>
2019-11-03 15:57:09 -0800

    Updates

--- precache-manifest.c4b97fc0de666780e38c040484f55389.js
+++ precache-manifest.c4b97fc0de666780e38c040484f55389.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3cf5ea882abf7685cdc5032a6f191664",
+    "revision": "85685a6ea3d790758014d5b011dcf053",
     "url": "/website/index.html"
   },
   {
-    "revision": "4156158515523b7f47c7",
+    "revision": "dd89d8d93d6eb7cd09ed",
     "url": "/website/static/css/main.3bc30e68.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/website/static/js/2.cbeab657.chunk.js"
   },
   {
-    "revision": "4156158515523b7f47c7",
-    "url": "/website/static/js/main.9ef8a2de.chunk.js"
+    "revision": "dd89d8d93d6eb7cd09ed",
+    "url": "/website/static/js/main.8e2f2886.chunk.js"
   },
   {
     "revision": "4289eddbccb8920ec418",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.26101b69cc23fe2a862d214a23171969.js"
+  "/website/precache-manifest.c4b97fc0de666780e38c040484f55389.js"
 );
 
 self.addEventListener('message', (event) => {
